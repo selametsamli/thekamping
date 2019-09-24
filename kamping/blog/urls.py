7b596400df5abebfc399cc46e6bc864978a06202
selametsamli <selamet96@gmail.com>
2019-09-24 19:12:02 +0300

    list, detail, update sayfaları oluşturuldu

--- kamping/blog/urls.py
+++ kamping/blog/urls.py
@@ -3,5 +3,8 @@ from blog import views
 
 urlpatterns = [
     path('post-create', views.post_create, name="post-create"),
+    path('post-list', views.post_list, name="post-list"),
+    path('post-update/<slug:slug>', views.post_update, name="post-update"),
+    path('post-detail/<slug:slug>', views.post_detail, name="post-detail"),
 
 ]
--- kamping/blog/views.py
+++ kamping/blog/views.py
@@ -1,12 +1,19 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from blog.forms import BlogForm
+from blog.models import Blog
 
 
+def post_list(request):
+    posts = Blog.objects.all()
+    return render(request, 'blog/post_list.html', context={'posts': posts})
+
+
+@login_required
 def post_create(request):
     form = BlogForm()
     if request.method == "POST":
@@ -17,3 +24,23 @@ def post_create(request):
             blog.save()
             return HttpResponseRedirect(blog.get_absolute_url())
     return render(request, 'blog/post_create.html', context={'form': form})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Blog, slug=slug)
+
+    return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+@login_required(login_url=reverse_lazy('user-login'))
+def post_update(request, slug):
+    blog = get_object_or_404(Blog, slug=slug)
+    if request.user != blog.author:
+        return HttpResponseForbidden()
+    form = BlogForm(instance=blog, data=request.POST or None, files=request.FILES or None)
+    if form.is_valid():
+        form.save()
+
+        return HttpResponseRedirect(blog.get_absolute_url())
+    context = {'form': form, 'blog': blog}
+    return render(request, 'blog/post_update.html', context=context)
