2bc196b8afe3587cd5753d8df4822438b578b067
selametsamli <selamet96@gmail.com>
2019-10-11 22:01:36 +0300

    Değerlendirmeyi yapacak kullanıcılar belirlendi

--- kamping/camp/decorators.py
+++ kamping/camp/decorators.py
@@ -1,17 +1,18 @@
-from django.http import HttpResponseBadRequest
 from django.shortcuts import reverse, HttpResponseRedirect, get_object_or_404
-from camp.models import Camp, CampParticipants
+from camp.models import Camp, CampParticipants, Feedback
+from django.http import HttpResponse
 
 
 def user_feedback_status(func):
     def wrap(request, *args, **kwargs):
         camp = get_object_or_404(Camp, slug=kwargs['slug'])
         camp_joined_list = CampParticipants.objects.filter(camp=camp, user=request.user)
-        if camp.status == 'yayında' or not camp_joined_list:
-            return HttpResponseRedirect(reverse('camp-list'))
-       # elif CampParticipants.objects.filter(camp=camp, user=request.user).exists():
-        #    return HttpResponseRedirect(reverse('camp-list'))
-
+        if camp.status == 'yayında':
+            return HttpResponse('Değerlendirme yapmak için kampın başlamasını veya bitmesini beklemelisiniz.')
+        elif Feedback.objects.filter(camp=camp, user=request.user).exists():
+            return HttpResponse('Zaten bir değerlendirme yapmışsınız.')
+        elif not camp_joined_list:
+            return HttpResponse('Bu kampa katılmadığınız için değerlendirme yapamazsınız.')
         return func(request, *args, **kwargs)
 
     return wrap
